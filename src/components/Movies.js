2e4b92643c604a437f790e9147ecbdff81992eef
Dismas Ng'oda <dismas.ngoda@student.moringaschool.com>
2022-10-02 20:19:24 +0300

    Completed assignment

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,21 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  // let movieCard = 
+  return (
+    <div>
+      {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map(movie => (
+    <div key={movie.title}>
+      Name: {movie.title}<br/>
+      Time: {movie.time}
+      <ul>
+        {movie.genres.map(genre => <li key={genre}>{genre}</li>)}
+      </ul>
+    </div>
+  ))}
+    </div>);
 }
 
 export default Movies;
